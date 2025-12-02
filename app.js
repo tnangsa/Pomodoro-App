074ad96cdb2209830bb49fa4cfe972326ac5355d
Tenzin <tnangsa@wgu.edu>
2025-12-02 16:33:39 -0500

    created a appTimer const, number in our session variable (25) will convert to a Number & be assigned to sessionAmount

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const startBtn = document.querySelector(".btn-start");
 const session = document.querySelector(".minutes");
 let myInterval;
 let state = true;
+
+const appTimer = () => {
+    const sessionAmount = Number.parseInt(session.textContent);
+}
