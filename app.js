d71f634925cb48463bbbe44e0642b822b3e485b0
Tenzin <tnangsa@wgu.edu>
2025-12-02 16:58:13 -0500

    gives us the seconds * min left, if its below 10 sec, it will add 0 to the front

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ const appTimer = () => {
 
             let minutesLeft = Math.floor(totalSeconds / 60);
             let secondsLeft = totalSeconds % 60;
+
+            if (secondsLeft < 10) {
+                secondDiv.textContent = "0" + secondsLeft;
+            }else {
+                secondDiv.textContent = secondsLeft;
+            } 
+            minuteDiv.textContent = `${minutesLeft}`;
         };
+        
         myInterval = setInterval(updateSeconds, 1000);
     }else {
         alert("Session has already started");
