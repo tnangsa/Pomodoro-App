220edeb26e912dd46085c7c8c9091a422588c853
Tenzin <tnangsa@wgu.edu>
2025-12-02 16:30:28 -0500

    created myInterval & state variables, state (if true) will mean timer will progress but if its set to false, it will alert to notify the session has already started

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const bells = new Audio("./sounds/bell.wav");
 const startBtn = document.querySelector(".btn-start");
 const session = document.querySelector(".minutes");
+let myInterval;
+let state = true;
